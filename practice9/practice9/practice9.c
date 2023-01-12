dba1ea34e4c43db48c9232a5c22b7b74ee9adf3f
OldSheep <120314201+OldSheep1@users.noreply.github.com>
2023-01-12 21:46:06 +0800

    计算100之内有多少个9-计算1/1+1/2-1/3......1/100-乘法表

---
+++ practice9/practice9/practice9.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)
+//		{
+//			count++;
+//		}
+//		if (i / 10 == 9)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+// //0000000000000000000计算100之内有多少个9  0000000000000
+//#include <stdio.h>
+//int mian()
+//{
+//	double sum = 0.0;
+//	int i = 0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag * 1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+//0000000000000000000000计算1/1+1/2-1/3......000000000000000
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	//行数
+//	for (i = 1; i <= 9; i++)
+//	{
+//		//打印一行
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf(" % d * %d = %-2d", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	
+//	return 0;
+//}
+//000000000000乘法表0000000000000000000
